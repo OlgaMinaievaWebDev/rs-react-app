798713de5faed0bc7d2fd992aaba4047804fa762
Olga <minaeva9@gmail.com>
2026-05-09 14:51:05 -0400

    test: add Search component tests

--- src/components/Search.test.tsx
+++ src/components/Search.test.tsx
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom';
+import { describe, expect, it, vi } from 'vitest';
+import { Search } from './Search';
+import { render, screen } from '@testing-library/react';
+
+describe('Search component', () => {
+  it('should render input and button', () => {
+    const mock = vi.fn();
+    render(<Search value="" onChange={mock} onSearch={mock} />);
+    const searchButton = screen.getByRole('button', { name: /search/i });
+    expect(searchButton).toBeInTheDocument();
+    const userInput = screen.getByRole('textbox');
+    expect(userInput).toBeInTheDocument();
+  });
+  it('should call onChange when user types', () => {});
+  it('should call onSearch when button clicked', () => {});
+});
