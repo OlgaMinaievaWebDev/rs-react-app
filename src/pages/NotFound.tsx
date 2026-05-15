c04d77021acae4ca5b81a13e0df4d9ddcb07fdbf
Olga <minaeva9@gmail.com>
2026-05-14 22:33:56 -0400

    feat: add 404 page with home navigation

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFound() {
-  return <div>Not Found</div>;
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Page not found</p>
+      <Link to={'/'}>Return Home</Link>
+    </div>
+  );
 }
+
